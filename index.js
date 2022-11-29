ddc4070f6293a8abe30baa557e599e5d3cb28b68
Gobi Mahato <116241775+gobiweb@users.noreply.github.com>
2022-11-29 14:04:13 +0000

    first home route created

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello to codespace");
+const app = require("./app");
+const { PORT } = process.env;
+
+app.listen(PORT, () => {
+  console.log(`SERVER is running on port: ${PORT}`);
+});
